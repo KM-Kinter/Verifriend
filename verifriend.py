1ad91d7bf1719c3156807e00625e2792f66039af
Kyryl Medvediev <134844241+KM-Kinter@users.noreply.github.com>
2023-11-29 07:45:42 +0100

    Update verifriend.py with more usable functions and autokick function

--- verifriend.py
+++ verifriend.py
@@ -1,70 +1,128 @@
 import discord
 from discord.ext import commands, tasks
 from datetime import datetime, timedelta, timezone
+import logging
 
-# Ustawienia intencji w celu uzyskania dostępu do informacji o członkach serwera
+# Setting up intents to access server member information
 intents = discord.Intents.default()
 intents.members = True
 
-# Inicjalizacja bota
+# Initializing the bot
 bot = commands.Bot(command_prefix='!', intents=intents)
-bot.remove_command("help")  # Usunięcie domyślnej komendy help
+bot.remove_command("help")  # Removing the default help command
 
-# Komenda help
+# Setting up the logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('verification_bot')
+
+# Bot token and role name for verification
+bot_token = None
+verification_role_name = None
+guild_id = None
+channel_id = None
+removal_channel_id = None
+
+# Help command
 @bot.command()
 async def help(ctx):
     await ctx.send("Usage: sending reminders to verify yourself")
 
-# Event on_ready - wywoływany po zalogowaniu bota
+
+# Event on_ready - triggered upon bot login
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    check_verification.start()  # Rozpoczęcie zadania cyklicznego
+    logger.info(f'Logged in as {bot.user.name}')
+    check_verification.start()  # Starting the periodic task
 
-# Zadanie cykliczne sprawdzające weryfikację użytkowników (co 24 godziny)
+
+# Function to set token, role name, server ID, channel ID, and removal channel ID
+def set_credentials():
+    global bot_token, verification_role_name, guild_id, channel_id, removal_channel_id
+    bot_token = input('Enter bot token: ')
+    verification_role_name = input('Enter verification role name: ')
+    guild_id = int(input('Enter guild ID: '))
+    channel_id = int(input('Enter channel ID: '))
+    removal_channel_id = int(input('Enter removal channel ID: '))
+
+
+# Periodic task checking user verification
 @tasks.loop(hours=24)
 async def check_verification():
     try:
-        guild_id = 1172249999966197775
         guild = bot.get_guild(guild_id)
 
         if guild is None:
-            print(f'Nie udało się znaleźć serwera o ID: {guild_id}')
+            logger.error(f'Failed to find a server with ID: {guild_id}')
             return
 
-        # Iteracja przez listę członków serwera
+        verification_role = discord.utils.get(guild.roles, name=verification_role_name)
+        removal_channel = bot.get_channel(removal_channel_id)
+
+        # Iterating through the server's member list
         for member in guild.members:
             join_date = member.joined_at
 
             if join_date is not None:
-                # Konwersja do strefy czasowej UTC
-                join_date_utc = join_date.replace(tzinfo=timezone.utc) if join_date.tzinfo is None else join_date.astimezone(timezone.utc)
+                join_date_utc = join_date.replace(
+                    tzinfo=timezone.utc) if join_date.tzinfo is None else join_date.astimezone(timezone.utc)
+                days_since_join = (datetime.utcnow().replace(tzinfo=timezone.utc) - join_date_utc).days
 
-                # Sprawdzenie, czy minęło 30 dni od dołączenia
-                if (datetime.utcnow().replace(tzinfo=timezone.utc) - join_date_utc) >= timedelta(days=30):
-                    # Sprawdzenie, czy użytkownik nie ma roli "weryfikacja" i nie jest botem
-                    is_verified = any(role.name.lower() == 'weryfikacja' for role in member.roles)
-                    is_bot = member.bot
-
-                    if not is_verified and not is_bot:
-                        # Wysłanie wiadomości prywatnej do użytkownika
-                        try:
-                            await member.send(
+                if days_since_join == 30:
+                    try:
+                        await member.send(
                                 'Cześć! Zauważyliśmy, że od pewnego czasu jesteś na Discord Hackerspace Trójmiasto, ale jak do tej pory nie akceptujesz naszego regulaminu. '
                                 'Aby zaakceptować regulamin (widoczny na kanale #cześć), wystarczy wejść na kanał #weryfikacja i wysłać wiadomość o treści "t". '
                                 'Niedługo będziemy usuwać wszystkie niezweryfikowane osoby, więc jeśli chcesz być częścią społeczności Hackerspace Trójmiasto, zweryfikuj się już teraz. '
                                 'Dzięki 🙂'
                             )
+                    except discord.HTTPException as e:
+                        logger.warning(f'Error sending a private message to {member}: {e}')
+
+                if days_since_join == 45:
+                    try:
+                        await member.send(
+                                'Cześć! Zauważyliśmy, że od pewnego czasu jesteś na Discord Hackerspace Trójmiasto, ale jak do tej pory nie akceptujesz naszego regulaminu. '
+                                'Aby zaakceptować regulamin (widoczny na kanale #cześć), wystarczy wejść na kanał #weryfikacja i wysłać wiadomość o treści "t". '
+                                'Za 15 dni będziemy usuwać wszystkie niezweryfikowane osoby, więc jeśli chcesz być częścią społeczności Hackerspace Trójmiasto, zweryfikuj się już teraz. '
+                                'Dzięki 🙂'
+                            )
+                    except discord.HTTPException as e:
+                        logger.warning(f'Error sending a private message to {member}: {e}')
+
+                if days_since_join == 54:
+                    try:
+                        await removal_channel.send(
+                            f'{member.mention} will be removed in 6 hours due to lack of verification.'
+                        )
+                    except discord.HTTPException as e:
+                        logger.error(f'Error sending a message: {e}')
+
+                if days_since_join == 60:
+                    try:
+                        await removal_channel.send(
+                            f'Member {member} has been removed due to lack of verification after 60 days.'
+                        )
+                        await member.kick(reason='Not verified within 60 days')
+                    except discord.HTTPException as e:
+                        logger.error(f'Error sending a message or kicking {member}: {e}')
+                    else:
+                        try:
+                            await removal_channel.send(
+                                f'Member {member} has been removed from the server.'
+                            )
                         except discord.HTTPException as e:
-                            print(f'Wystąpił błąd podczas wysyłania wiadomości prywatnej do {member}: {e}')
+                            logger.error(f'Error sending a message: {e}')
+
+        await remove_unverified_members(guild, verification_role)  # Remove unverified members after 60 days
 
     except Exception as e:
-        print(f'Wystąpił błąd w funkcji check_verification: {e}')
+        logger.error(f'Error in the check_verification function: {e}')
 
-# Event on_member_join - wywoływany po dołączeniu nowego członka
-@bot.event
-async def on_member_join(member):
-    print(f'{member} dołączył do serwera.')
 
-# Uruchamianie bota
-bot.run('TOKEN_BOTA')
+# Running the bot
+if __name__ == "__main__":
+    set_credentials()
+    if bot_token:
+        bot.run(bot_token)
+    else:
+        logger.error('Bot token is missing. Please provide the bot token.')
